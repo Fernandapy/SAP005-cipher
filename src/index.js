fc3125ad684429398c1ead6d63e2c9e2c81bd91a
Fernanda Santos <fernanda.idt@gmail.com>
2020-11-07 18:43:19 -0300

    Adiciona todo projeto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+import cipher from './cipher.js';
+const buttonCodificar = document.getElementById("codificar");
+buttonCodificar.addEventListener("click", codificar);
+function codificar() {
+    
+}
